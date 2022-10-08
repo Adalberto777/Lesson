8eb687538069c12dbeb4d3918ca6682244d02e57
Adalberto777 <albert.tliashev@gmail.com>
2022-10-08 13:13:54 +0500

    lesson1

---
+++ lesson1/lec.py
@@ -0,0 +1,35 @@
+# print('Hello World')
+# value = None
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 1234
+# print(value)
+# s = 'Hello World'
+# print(s) # вывод строки
+# d = "Hello ' World"
+# e = 'Hello "World'
+# print(d)
+# print(e)
+# f = 'Hello \nWorld'
+# print(f)
+# print(a, b, f)
+# print(a, '-', b, '-', e)
+# print('{}-{}-{}'.format(a, b, e))
+# print(f'{a}-{b}-{e}')
+# print('{1}-{0}-{2}'.format(a, b, e))
+# list = ['1', '2', '3', 'hello', 1, 2]
+# print(list)
+print('Enter a')
+a = input()
+print('Enter b')
+b = input()
+print(a, '+', b, '=', a+b)
+print('Enter c')
+c = int(input())
+print('Enter d')
+d = int(input())
+print(c, '+', d, '=', c+d)
+f = 1 < 2 or 3 > 7
+print(f)
